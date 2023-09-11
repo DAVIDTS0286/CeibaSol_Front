98178a69b65133742282c9c5414d20b1bd2f1cec
David Tamayo <david.tamayo@uptc.edu.co>
2023-09-11 00:33:52 -0500

    Nueva versión del inicio para contener el apartado de videojuego y creacion de gamesection para el iframe

--- src/components/menu/GameSection.js
+++ src/components/menu/GameSection.js
@@ -1,20 +1,32 @@
 import React from "react";
 
 function GameSection() {
-  return (
-    <div className="py-6">
-      <h2 className="text-2xl font-bold text-gray-800">Introducción al Videojuego</h2>
-      <p className="text-gray-600">
-        Aquí puedes agregar una breve descripción o introducción sobre el videojuego que deseas presentar en tu página web.
-      </p>
-      <iframe
-        src="/game.html" // Asegúrate de ajustar la ruta correcta
-        //src="/Game/fondoInicio.jpeg" // Asegúrate de ajustar la ruta correcta
-        //src="https://github.com/CodyAbode/Dungeon-Blox" // Asegúrate de ajustar la ruta correcta
+  const iframeStyle = {
+    width: "100%", // Utiliza el ancho completo del contenedor
+    height: "600px", // Altura fija o ajusta según tus necesidades
+    border: "1em", // Quita el borde del iframe
+    borderRadius: "0.5em", // Agrega bordes redondeados
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)", // Agrega una sombra
+  };
 
-        width="800"
-        height="600"
+  return (
+    <div className="py-6 container text-center">
+      <h1 className="my-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-lime-600 from-lime-900">
+          ¿Qué hacemos?
+        </span>
+      </h1>
+
+      <p className="text-gray-600 text-size-40">
+        ¡Hola! ¿Quieres saber más acerca de nuestra empresa?
+        <br />
+        Hazlo de una forma interactiva con nuestro videojuego!!
+      </p>
+      <br/>
+      <iframe
+        src="http://localhost:4000/src/components/menu/Game/game.html"
         title="Videojuego Unity"
+        style={iframeStyle} // Aplica los estilos al iframe
       />
     </div>
   );
